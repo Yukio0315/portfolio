dee255d7e2d9da2c5088f77045354154e14fa9c1
Yukio Ueda <triangle.pillow@gmail.com>
2020-06-01 15:23:47 +0900

    add animation for drawer menu

--- src/components/Navigation/TheSideMenu.vue
+++ src/components/Navigation/TheSideMenu.vue
@@ -1,29 +1,27 @@
 <template>
   <nav class="menu" @mouseover="openMenu" @mouseleave="closeMenu">
-    <div class="menu-bar">
+    <div class="bar">
       <font-awesome-icon
-        class="menu-bar-icon"
+        class="icon"
         :icon="isClose ? 'bars' : 'times'"
         @click="toggleMenu"
       />
     </div>
-    <div v-if="!isClose" class="menu-open">
-      <ul class="menu-open-list">
+    <div class="menu__open">
+      <ul v-show="!isClose" class="list">
         <li v-for="item in menu" :key="item.id" @click="chooseMenu(item.name)">
           {{ item.name }}
         </li>
       </ul>
-      <div class="menu-open-icon">
-        <a :href="`mailto:${email}`"
-          ><font-awesome-icon
-            class="menu-open-icon-envelope"
-            :icon="['far', 'envelope']"
-        /></a>
-        <a :href="github"
-          ><font-awesome-icon
-            class="menu-open-icon-github"
-            :icon="['fab', 'github']"
-        /></a>
+      <div v-show="!isClose">
+        <div class="social-icons">
+          <a :href="`mailto:${email}`"
+            ><font-awesome-icon class="envelope" :icon="['far', 'envelope']"
+          /></a>
+          <a :href="github"
+            ><font-awesome-icon class="github" :icon="['fab', 'github']"
+          /></a>
+        </div>
       </div>
     </div>
   </nav>
@@ -31,6 +29,7 @@
 
 <script lang="ts">
 import { Vue, Component, Prop, Emit } from 'nuxt-property-decorator'
+import anime from 'animejs'
 
 type Menu = [
   {
@@ -48,14 +47,56 @@ export default class TheSideMenu extends Vue {
 
   closeMenu() {
     this.isClose = true
+    this.fadeMenu()
+    this.hideItem('.list')
+    this.hideItem('.social-icons')
   }
 
   openMenu() {
     this.isClose = false
+    this.drawMenu()
+    this.showItem('.list')
+    this.showItem('.social-icons')
   }
 
   toggleMenu() {
     this.isClose = !this.isClose
+    if (this.isClose) this.fadeMenu()
+    else this.openMenu()
+  }
+
+  drawMenu() {
+    anime({
+      targets: '.menu__open',
+      width: '150px',
+      duration: 500,
+      easing: 'easeInOutExpo'
+    })
+  }
+
+  fadeMenu() {
+    anime({
+      targets: '.menu__open',
+      width: '0px',
+      duration: 500,
+      easing: 'easeInOutExpo'
+    })
+  }
+
+  showItem(targets: string) {
+    anime({
+      targets,
+      opacity: 1,
+      duration: 500,
+      delay: 500
+    })
+  }
+
+  hideItem(targets: string) {
+    anime({
+      targets,
+      opacity: 0
+    })
   }
 
   @Emit()
@@ -76,44 +117,42 @@ export default class TheSideMenu extends Vue {
   right: 0;
   color: $color-main;
   background-color: $color-font;
-  &-open {
+  .bar {
+    display: flex;
+    $icon-size: 15%;
+    width: $width-side;
+    justify-content: center;
+    .icon {
+      height: 100%;
+      width: $icon-size;
+      cursor: pointer;
+    }
+  }
+  &__open {
     display: grid;
     grid-template-rows: 80% 20%;
-    width: $width-side-open;
-    margin: {
-      top: 40%;
-      left: 50px;
-    }
-    &-list {
+    width: 0;
+    .list {
       grid-row: 1 / 2;
       font-size: 26px;
       list-style-type: none;
       align-self: center;
+      opacity: 0;
       li {
         margin-top: 40px;
         cursor: pointer;
       }
     }
-    &-icon {
+    .social-icons {
       grid-row: 2 / 3;
       width: 200%;
-      &-envelope,
-      &-github {
+      opacity: 0;
+      .envelope,
+      .github {
         margin-left: 40px;
         font-size: 40px;
         color: $color-main;
       }
-    }
-  }
-  &-bar {
-    display: flex;
-    $icon-size: 15%;
-    width: $width-side;
-    justify-content: center;
-    &-icon {
-      height: 100%;
-      width: $icon-size;
-      cursor: pointer;
     }
   }
 }
